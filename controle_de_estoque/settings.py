fa638d0fd201d90a8a62ad41f4c4568700ffc655
Marcelo Mizuno <marcelo@hotmail.co.jp>
2024-10-13 17:37:50 -0300

    Added requirements.txt

--- controle_de_estoque/settings.py
+++ controle_de_estoque/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,11 +76,11 @@ WSGI_APPLICATION = "controle_de_estoque.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
 
 
